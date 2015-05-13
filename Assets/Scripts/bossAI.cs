02512fce710a2925d6c583000b99b8373fe97e5a
Sense <gryphsoft@live.com>
2015-05-13 10:50:37 +0800

    boss projectiles

---
+++ Assets/Scripts/bossAI.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bossAI : MonoBehaviour {
+	GameObject GOprojectile;
+	GameObject GOrain;
+	bool isShooting;
+	public float maxShootCD;
+	float shootCD;
+	public float maxShootInterval;
+	float shootInterval;
+	public float speed;
+	float angle;
+
+	public float maxRainInterval;
+	float rainInterval;
+	bool isRain;
+
+	
+	// Use this for initialization
+	void Start () {
+		shootCD = maxShootCD;
+		shootInterval = maxShootInterval;
+		angle = 0;
+	}
+
+	void RangeAttack() 
+	{
+		//GOprojectile.GetComponent<Animator>().SetBool("isShooting", true);
+		//Debug.Log (shootInterval);
+		shootCD -= Time.deltaTime;
+		if (shootCD <= 0.0f)
+		{
+			isShooting = true;
+		}
+		//Debug.Log (angle);
+		if (isShooting) //if (Input.GetKeyUp("a"))
+		{
+			shootInterval -= Time.deltaTime;
+			if (shootInterval <= 0.0f)
+			{
+				GOprojectile = Instantiate(Resources.Load("projectiles/Prefab/bossProj"), new Vector3(transform.position.x, 
+				             		       transform.position.y + 2.0f, -0.1f), Quaternion.identity) as GameObject;
+				
+				GOprojectile.GetComponent<Animator>().speed = 0.3f;
+				GOprojectile.transform.position = new Vector3(GOprojectile.transform.position.x, GOprojectile.transform.position.y, -0.1f);
+				
+				//int angle = Random.Range(0, 11) * 30;
+				
+				GOprojectile.GetComponent<bossProj>().SetDirection(Mathf.Cos(Mathf.PI / 180 * angle * 30) * speed * Time.deltaTime, 
+				                                                 Mathf.Sin(Mathf.PI / 180 * angle * 30) * speed * Time.deltaTime);
+				
+				shootInterval = maxShootInterval;
+				angle++;
+			}
+		}
+		
+		if (angle > 11)
+		{
+			angle = 0;
+			isShooting = false;
+			shootCD = maxShootCD;
+		}
+	}
+
+	void RainEvent()
+	{
+		/*rainInterval -= Time.deltaTime;
+		if (rainInterval < maxRainInterval)
+		{
+			isRain = true;
+		}
+		
+		GOrain = Instantiate(Resources.Load("env/Prefab/rain")) as GameObject;*/
+	}
+
+	// Update is called once per frame
+	void Update () {
+		RangeAttack();
+		RainEvent();
+	}
+}
---
+++ Assets/Scripts/bossProj.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bossProj : MonoBehaviour {
+	private float dirX;
+	private float dirY;
+
+	public void SetDirection(float _dirX, float _dirY)
+	{
+		dirX = _dirX;
+		dirY = _dirY;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(dirX, dirY, 0.0f);
+
+		//Debug.Log(transform.position.x + " " + transform.position.y);
+		if (transform.position.x < -12.0f || transform.position.x > 12.0f ||
+		    transform.position.y < -6.0f || transform.position.y > 6.0f)
+		{
+			//Debug.Log("DestROYED");
+			Destroy(gameObject);
+		}
+	}
+}
