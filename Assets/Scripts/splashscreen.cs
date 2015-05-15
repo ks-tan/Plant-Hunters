f9831139c0ffade5ec98691cdb684618707c80b2
Jmyn <jermyntanu92@gmail.com>
2015-05-15 10:09:44 +0800

    Splashscreen

---
+++ Assets/Scripts/splashscreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class splashscreen : MonoBehaviour {
+    public AudioClip[] sfx;
+
+	// Use this for initialization
+	void Start () {
+        audio.clip = sfx[0];
+        audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel("Main Scene2");
+        }
+	}
+}
